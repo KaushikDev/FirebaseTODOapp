5ab5927059eaea5d658e1108ce3725374a1fd023
Piyush Kaushik <piyush.kaushik02@gmail.com>
2017-06-28 11:21:54 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
   // Initialize Firebase
   const config = {
     apiKey: "AIzaSyBZwn4c0KmEGXXFScg_c8aLTte1CRmTknY",
-    authDomain: "kaushikdev.github.io",
+    authDomain: "todo-app-612b7.firebaseapp.com",
     databaseURL: "https://todo-app-612b7.firebaseio.com",
     storageBucket: "todo-app-612b7.appspot.com",
     messagingSenderId: "439347897690"
